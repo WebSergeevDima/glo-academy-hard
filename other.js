0dafdee4d3a86ce77d3ffb73b0e1a9da707c6934
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-01-28 19:06:04 +0300

    fixed multiplication

--- other.js
+++ other.js
@@ -1,11 +1,11 @@
 let n = 266219;
 
-let sum = String(n).split('').reduce((acc, num) => {
-  return acc += +num;
-}, 0);
+let multiplication = String(n).split('').reduce((acc, num) => {
+  return acc *= +num;
+}, 1);
 
-console.log('Произведение (умножение) цифр: ', sum);
+console.log('Произведение (умножение) цифр: ', multiplication);
 
-let powSum = sum ** 3;
+let powSum = multiplication ** 3;
 
 console.log('Первые 2 цифры: ', +String(powSum).slice(0, 2));
