d0bcc7a4473e0f4ff6744cbadc9a5a2301eb634d
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-01-31 19:52:40 +0300

    fixed bug lesson3

--- lesson03.js
+++ lesson03.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const lang = prompt('Введите "ru" или "en"');
+const namePerson = prompt('Введите имя');
 
 // 1) через if
 
@@ -16,34 +17,20 @@ if (lang === 'ru') {
 // 2) через switch-case
 switch (lang) {
     case 'ru':
-        console.log('Пн');
-        console.log('Вт');
-        console.log('Ср');
-        console.log('Чт');
-        console.log('Пт');
-        console.log('Сб');
-        console.log('Вс');
+        console.log(ruDays.join(', '));
         break;
     case 'en':
-        console.log('Mo');
-        console.log('Tu');
-        console.log('We');
-        console.log('Th');
-        console.log('Fr');
-        console.log('Sa');
-        console.log('Su');
+        console.log(enDays.join(', '));
 };
 
 // 3) через многомерный массив
-const days = [
-    ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-    ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
-];
+const days = new Map([
+    ['ru', ruDays],
+    ['en', enDays],
+]);
 
-console.log(lang === 'ru' ? days[0].join(', ') : days[1].join(', '));
+console.log(days.get(lang).join(', '));
 
 // 4) namePerson
-
-const namePerson = prompt('Введите имя');
 
 console.log(namePerson === 'Артем' ? 'директор' : namePerson === 'Александр' ? 'преподаватель' : 'студент');
